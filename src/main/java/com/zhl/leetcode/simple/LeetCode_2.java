acbd14455e60a366fcd3be9d003198842763151e
zhuhailong <13121141179@163.com>
2019-11-14 10:16:26 +0800

    初级：LeetCode LCP 7

--- src/main/java/com/zhl/leetcode/simple/LeetCode_2.java
+++ src/main/java/com/zhl/leetcode/simple/LeetCode_2.java
@@ -7,29 +7,78 @@ package com.zhl.leetcode.simple;
  * @create: 2019/11/13 08:45
  */
 public class LeetCode_2 {
-
+    /**
+     * 重做
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // 2 4 3  = 342
         // 5 6 4  = 465
-        //
 
-        return null;
+        // 这道题，只能采用前序遍历，不能采用后续遍历，因为两个数字的长度可能是不对应的
+        // 采用for循环的问题，主要在于每次遍历都是独立的，所以不能构建出新ListNode的next关系
+        // 可以采用前序遍历+后续遍历的结合
+
+        ListNode node = new ListNode(0);
+        addTwoNumbers(node, l1, l2, 0);
+        node = node.next;
+        return node;
+    }
+
+    public int addTwoNumbers(ListNode node, ListNode l1, ListNode l2, int ten) {
+        if (l1 == null && l2 == null) {
+            return 0;
+        }
+
+        // 获取 num1
+        ListNode firstParam;
+        int num1 = 0;
+        if (l1 != null) {
+            num1 = l1.val;
+            firstParam = l1.next;
+        } else {
+            firstParam = null;
+        }
+
+        // 获取 num2
+        ListNode secondParam;
+        int num2 = 0;
+        if (l2 != null) {
+            num2 = l2.val;
+            secondParam = l2.next;
+        } else {
+            secondParam = null;
+        }
+
+        int num = num1 + num2 + ten;
+        if (num >= 10) {
+            ListNode temp = new ListNode(num - 10);
+            node.next = temp;
+            if (firstParam == null && secondParam == null) {
+                temp.next = new ListNode(1);
+            }
+            addTwoNumbers(temp, firstParam, secondParam, 1);
+        } else {
+            ListNode temp = new ListNode(num);
+            node.next = temp;
+            addTwoNumbers(temp, firstParam, secondParam, 0);
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
         LeetCode_2 leetCode = new LeetCode_2();
 
-        ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(4);
-        ListNode node3 = new ListNode(3);
-        node1.next = node2;
-        node2.next = node3;
+        ListNode node1 = new ListNode(5);
+        //ListNode node2 = new ListNode(8);
+        //ListNode node3 = new ListNode(3);
+        //node1.next = node2;
+        //node2.next = node3;
 
         ListNode node4 = new ListNode(5);
-        ListNode node5 = new ListNode(6);
-        ListNode node6 = new ListNode(4);
-        node4.next = node5;
-        node5.next = node6;
+        //ListNode node5 = new ListNode(6);
+        //ListNode node6 = new ListNode(4);
+        //node4.next = node5;
+        //node5.next = node6;
 
         ListNode result = leetCode.addTwoNumbers(node1, node4);
         System.out.println(result);
---
+++ src/main/java/com/zhl/leetcode/simple/LeetCode_7.java
@@ -0,0 +1,34 @@
+package com.zhl.leetcode.simple;
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2019/11/14 08:31
+ */
+public class LeetCode_7 {
+
+    /**
+     * 这道题，一般有两种解法：
+     * 1、通过数学公式去解决
+     * 2、通过字符串的反转来解决
+     * 在我看来，既然是数字的反转，所以，这里采用通过数学公式来解决
+     *
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        long result = 0;
+
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+        return (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) ? 0 : (int) result;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_7 leetCode = new LeetCode_7();
+        System.out.println(leetCode.reverse(1230));
+    }
+}
