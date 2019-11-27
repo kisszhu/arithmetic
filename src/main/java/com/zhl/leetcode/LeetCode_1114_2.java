b5524b9e3e656cdeac1d10f249384f5a18e85558
zhuhailong <13121141179@163.com>
2019-11-27 09:58:06 +0800

    初级：LeetCode LCP 1114 使用CountDownLatch控制线程的执行顺序 LCP 1114_2 使用成员遍历Object，结合synchronized来做屏障，在屏障内，判断标识的取值，进行wait，最后执行notifyAll LCP 1114_3 使用volatile这个java可见性的特性，来做到多个线程的变量可见性

---
+++ src/main/java/com/zhl/leetcode/LeetCode_1114_2.java
@@ -0,0 +1,59 @@
+package com.zhl.leetcode;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2019/11/27 09:06
+ */
+public class LeetCode_1114_2 {
+
+    /**
+     * 起到标识的作用
+     */
+    private boolean second = false;
+    private boolean third = false;
+
+    /**
+     * 屏障
+     */
+    private Object lock = new Object();
+
+    public LeetCode_1114_2() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (lock) {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            second = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (lock) {
+            while (!second) {
+                lock.wait();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            third = true;
+            lock.notifyAll();
+        }
+
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (lock) {
+            while (!third) {
+                lock.wait();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+        }
+    }
+}
--- src/main/java/com/zhl/leetcode/LeetCode_1114_3.java
+++ src/main/java/com/zhl/leetcode/LeetCode_1114_3.java
@@ -1,39 +1,37 @@
 package com.zhl.leetcode;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * @program arithmetic
  * @description:
  * @author: meilong
  * @create: 2019/11/27 09:06
  */
-public class LeetCode_1114 {
-    private CountDownLatch second = new CountDownLatch(1);
-    private CountDownLatch third = new CountDownLatch(1);
+public class LeetCode_1114_3 {
 
-    public LeetCode_1114() {
+    public LeetCode_1114_3() {
 
     }
 
-    public void first(Runnable printFirst) throws InterruptedException {
+    private volatile int flag = 1;
 
+    public void first(Runnable printFirst) throws InterruptedException {
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        second.countDown();
-
+        flag++;
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        second.await();
+        while (flag != 2) {
+
+        }
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
-        third.countDown();
-
+        flag++;
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        third.await();
+        while (flag != 3) {
+        }
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
     }
