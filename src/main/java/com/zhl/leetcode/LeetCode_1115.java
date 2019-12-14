b0402c8662d4c04bb637824ddfcfed2d18b65b6f
zhuhailong <13121141179@163.com>
2019-12-14 10:00:56 +0800

    中级：交替打印FooBar

---
+++ src/main/java/com/zhl/leetcode/LeetCode_1115.java
@@ -0,0 +1,79 @@
+package com.zhl.leetcode;
+
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2019/12/14 08:52
+ */
+public class LeetCode_1115 {
+
+    private int n;
+
+    public LeetCode_1115(int n) {
+        this.n = n;
+    }
+
+    private volatile boolean flag = false;
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            while (flag) {
+                Thread.yield();
+            }
+            printFoo.run();
+            flag = true;
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            while (!flag) {
+                Thread.yield();
+            }
+            printBar.run();
+            flag = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        LeetCode_1115 test = new LeetCode_1115(10);
+        test.foo(new Runnable() {
+            @Override
+            public void run() {
+                test.foo();
+            }
+        });
+
+        test.bar(new Runnable() {
+            @Override
+            public void run() {
+                test.bar();
+            }
+        });
+    }
+
+    public void foo() {
+        System.out.println("foo");
+
+    }
+
+    public void bar() {
+        System.out.println("bar");
+
+    }
+}
+/**
+ * 看到先输出foo，在输出bar，就可以知道，这个是一个线程屏障问题
+ * 第一种方式是通过CountDownLatch设置屏障，这个是Java自带的
+ * 第二种方式是通过设置一个Object类型的成员变量，辅以synchronized，通过wait & notifyAll进行控制
+ * 第三种方式是通过volatile这个特性，这个特性可以保证可见性
+ */
+
+//通过CountDownLatch & Object synchronized
+//第一种方式是通过CountDownLatch设置屏障，这个是Java自带的。
+//        第二种方式是通过Object这个成员变量辅以synchronzied，通过wait & notifyAll.
+//        第三种方法是通过volatile这个特性,这个特性可以保证可见性，故全局唯一
