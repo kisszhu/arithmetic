f0bf13d6e7ff2afc01b2f7020c331f9b4c2dc8cb
zhuhailong <13121141179@163.com>
2019-12-19 09:39:59 +0800

    Hash：

---
+++ src/main/java/com/zhl/leetcode/hash/LeetCode_771.java
@@ -0,0 +1,36 @@
+package com.zhl.leetcode.hash;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2019/12/19 09:22
+ */
+public class LeetCode_771 {
+
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>(16);
+        for (char i : J.toCharArray()) {
+            set.add(i);
+        }
+        int num = 0;
+        for (char i : S.toCharArray()) {
+            if (set.contains(i)) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_771 test = new LeetCode_771();
+        int result = test.numJewelsInStones("z", "ZZZ");
+        System.out.println(result);
+    }
+}
