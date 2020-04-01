4aec77d4b9d07c40dc5a0d959b2e30d5d0812642
zhuhailong <13121141179@163.com>
2020-04-01 10:57:30 +0800

    num & 1  == num %2

--- src/main/java/com/zhl/leetcode/stack/LeetCode_1111.java
+++ src/main/java/com/zhl/leetcode/stack/LeetCode_1111.java
@@ -12,9 +12,8 @@ public class LeetCode_1111 {
         int[] ans = new int[seq.length()];
         int idx = 0;
         for (char c : seq.toCharArray()) {
-            ans[idx++] = c == '(' ? idx & 1 : ((idx + 1) & 1);
-            System.out.println(idx & 1);
-            System.out.println((idx + 1) & 1);
+            ans[idx++] = c == '(' ? idx % 2 : ((idx + 1) % 2);
+            // ans[idx++] = c == '(' ? idx & 1 : ((idx + 1) & 1);
         }
         return ans;
     }
