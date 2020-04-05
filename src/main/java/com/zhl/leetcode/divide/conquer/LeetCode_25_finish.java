047f06ceba9243316b6a1184481f9715a582c211
zhuhailong <13121141179@163.com>
2020-04-05 14:31:10 +0800

    分治，两个排序后的链表，合并

--- src/main/java/com/zhl/leetcode/divide/conquer/LeetCode_25_finish.java
+++ src/main/java/com/zhl/leetcode/divide/conquer/LeetCode_25_finish.java
@@ -6,8 +6,13 @@ package com.zhl.leetcode.divide.conquer;
  * @author: meilong
  * @create: 2020/04/04 20:43
  */
-public class LeetCode_25 {
+public class LeetCode_25_finish {
 
+    /**
+     * 引入伪头节点：初始化一个辅助节点作为合并链表的头节点，循环开始将各节点添加至此辅助节点之后
+     * 本文引入伪头节点，以提升代码的简洁性和可读性
+     * 初始化：伪头节点head(作为整个合并链表的头部)
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(-1);
         ListNode dummy = head;
@@ -57,7 +62,7 @@ public class LeetCode_25 {
         listB1.next = listB2;
         listB2.next = listB4;
 
-        LeetCode_25 test = new LeetCode_25();
+        LeetCode_25_finish test = new LeetCode_25_finish();
         ListNode list = test.mergeTwoLists(listA1, listB1);
 
         while (list != null) {
