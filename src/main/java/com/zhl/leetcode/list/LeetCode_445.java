5f697e1a719b624d9795cc00df76d51d63ba256f
zhuhailong <13121141179@163.com>
2020-04-14 09:39:46 +0800

    两个链表数值相加

---
+++ src/main/java/com/zhl/leetcode/list/LeetCode_445.java
@@ -0,0 +1,60 @@
+package com.zhl.leetcode.list;
+
+import com.zhl.algorithms.chapter1_3.Stack;
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2020/04/14 09:01
+ */
+public class LeetCode_445 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        Stack<Integer> stack1 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        Stack<Integer> stack2 = new Stack<>();
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry > 0) {
+            int sum = carry;
+            sum += stack1.isEmpty() ? 0 : stack1.pop();
+            sum += stack2.isEmpty() ? 0 : stack2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+        return head;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
