7a35a5a3647b19d8d5656260ed7233c7a8ddc514
zhuhailong <13121141179@163.com>
2020-04-27 11:52:24 +0800

    二分查找

--- src/main/java/com/zhl/leetcode/binary/search/LeetCode_153.java
+++ src/main/java/com/zhl/leetcode/binary/search/LeetCode_153.java
@@ -13,15 +13,16 @@ public class LeetCode_153 {
          * 寻找最小值的题目，可以用二分查找来解决：
          * 用二分法查找，需要始终将目标值（这里是最小值）套住，并不断收缩左边界或右边界。
          */
-
         int left = 0;
         int right = nums.length - 1;
         //
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[right]) {
+                // 收缩左边界
                 left = mid + 1;
             } else if (nums[mid] < nums[right]) {
+                // 收缩右边界
                 right = mid;
             }
         }
