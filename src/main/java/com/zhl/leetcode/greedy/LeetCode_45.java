4572e7e9c6bcaffc14c69f9383f17a90c0c44d63
zhuhailong <13121141179@163.com>
2020-05-04 11:14:22 +0800

    贪心算法

---
+++ src/main/java/com/zhl/leetcode/greedy/LeetCode_45.java
@@ -0,0 +1,29 @@
+package com.zhl.leetcode.greedy;
+
+/**
+ * @program arithmetic
+ * @description:
+ * @author: meilong
+ * @create: 2020/05/04 11:13
+ */
+public class LeetCode_45 {
+
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
