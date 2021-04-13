c69dbfd3c7fed31b98c001c19dd9191fa29985fe
yuguang <zhuhailong@didiglobal.com>
2021-04-13 19:30:44 +0800

    535 Map Key 数字 加密 替换

--- src/main/java/com/zhl/leetcode/math/LeetCode_535.java
+++ src/main/java/com/zhl/leetcode/math/LeetCode_535.java
@@ -1,7 +1,8 @@
 package com.zhl.leetcode.math;
 
-import com.zhl.learn.algorithm.hash.map.level1.HashMap;
-import com.zhl.learn.algorithm.hash.map.level1.Map;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program arithmetic
@@ -11,23 +12,23 @@ import com.zhl.learn.algorithm.hash.map.level1.Map;
  */
 public class LeetCode_535 {
 
-    /**
-     * 加密算法是不可能将原始数据变少的，因此短网址服务需要建立数据库映射，要考虑重复性、高效加密等问题
-     * 这道题看了下LeetCode上的解法，发现小伙伴们的思维都好灵活
-     */
-    Map<String, String> kv = new HashMap<>();
+	Map<Integer, String> kv = new HashMap<>();
+	int i = 0;
 
-    // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {
-        return null;
-    }
+	public String encode(String longUrl) {
+		kv.put(i, longUrl);
+		return "http://tinyurl.com/" + i++;
+	}
 
-    // Decodes a shortened URL to its original URL.
-    public String decode(String shortUrl) {
-        return null;
-    }
+	public String decode(String shortUrl) {
+		return kv.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+	}
 
-    public static void main(String[] args) {
-        LeetCode_535 test = new LeetCode_535();
-    }
+	public static void main(String[] args) {
+		LeetCode_535 test = new LeetCode_535();
+		System.out.println(test.decode(test.encode("https://")));
+	}
 }
+
+
+
